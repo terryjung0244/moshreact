5235184b55de221b16dcad4e2b9b9db28b6d4431
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-20 22:14:50 -0400

    Arrow Functions

--- src/App.js
+++ src/App.js
@@ -24,16 +24,41 @@ const App = () => {
   // console.log(person.name);
 
   //The this keyword
-  const person = {
-    name: 'Terry',
-    eat() {
-      console.log(this);
-    }
-  };
+  // const person = {
+  //   name: 'Terry',
+  //   eat() {
+  //     console.log(this);
+  //   }
+  // };
 
-  person.eat();
-  const eat = person.eat.bind(person);
-  eat();
+  // person.eat();
+  // const eat = person.eat.bind(person);
+  // eat();
+
+  //arrow function
+  const square = function (number) { //function
+    return number * number;
+  }
+
+  const square2 = number => {
+    return number * number;
+  }
+
+  const square3 = number => number * number;
+  console.log(square3(3));
+
+  //example of useful way to use an arrow function
+  const jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: true },
+    { id: 3, isActive: false },
+  ]
+
+  const activejobs = jobs.filter(function(job) { return job.isActive});
+  console.log(activejobs);
+  const activejobs2 = jobs.filter(job => job.isActive);
+  console.log(activejobs2);
+
 
   return (
     <div>
